440b693d836cb75a5a89152080cd60a9f33e54ce
Jihoon Nam <mpq05@naver.com>
2021-08-01 00:11:29 +0900

    no.4948

--- 4948/_4948.py
+++ 4948/_4948.py
@@ -10,22 +10,34 @@ Original file is located at
 # 4948 베르트랑 공준
 import sys
 
+def is_prime(n): # 소수 판정
+    # 1의 경우는 취급 x
+    for i in range(2, int(n**0.5) + 1):
+        if n%i == 0:
+            return False
+    
+    return True
+        
+def find_all(): # 범위 내 소수 다 찾기
+    primes = []
+
+    for i in range(2,246912):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+primes = find_all()
+
 while True:
     n = int(sys.stdin.readline())
+    cnt = 0
 
     if n == 0: # 입력이 0 이면 멈춤
         break
 
-    cnt = 0
-
-    for i in range(n+1, 2*n+1):
-        if i == 2:
+    for i in primes:
+        if n < i <= n*2:
             cnt += 1
-        else:
-            for j in range(2, int(i**0.5) + 1):
-                if i % j == 0:
-                    break
-            else:
-                cnt += 1    
-
     print(cnt)
+
+int(2**0.5) + 1
